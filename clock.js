af858489999502401d6ecbf502ee4b25f3f11e24
paul657 <nestoripaul@gmail.com>
2016-02-03 11:15:45 +0200

    clock new

--- clock.js
+++ clock.js
@@ -1,21 +1,23 @@
 window.onload = function(){
-  setInterval(function(){
-    var date = new Date();
-    var hour = addZero(date.getHours());
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
-    second = addZero(second);
-
-    document.getElementById('clock').innerHTML = hour + ':' + addZero(minute) + ':' + second;
-
-    function addZero(number){
-      if(number < 10){
-        number = "0" + number;
-      }
-      return number;
-    }
-
-
-  }, 1000);
+  write();
+  setInterval(function(){write();}, 1000);
 
 };
+
+function write(){
+  var date = new Date();
+  var hour = addZero(date.getHours());
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  second = addZero(second);
+
+  document.getElementById('clock').innerHTML = hour + ':' + addZero(minute) + ':' + second;
+
+  function addZero(number){
+    if(number < 10){
+      number = "0" + number;
+    }
+    return number;
+  }
+
+}
